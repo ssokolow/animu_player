efc3b07bbba04bedff98b4a30a0b717cf4b50c5a
Stephan Sokolow <via_bzr.zen.ssokolow@spamgourmet.com>
2007-06-08 08:01:14 -0400

    Barring bugs, all that's left before version 1 is fleshing out the keySyms table.

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 TODO:
-- Use window.set_title() to set the Window title to the filename of the video being played.
-
+- I'm not sure what caused it, but something can cause the GUI on this to crash without taking MPlayer along.
+- Figure out how the heck to set up a proper fullscreen/unfullscreen toggle using PyGTK's wonky methods and events.
+- Build up a mapping table linking MPlayer names to keycodes.
 """
 
-TICK_INTERVAL = 300
+TICK_INTERVAL = 500
 START_SIZE = (640, 480)
+
+keySyms={
+		32: "SPACE",
+		102: "f",
+		113: "q",
+		65288: "BS",
+		65293: "ENTER",
+		65307: "ESC",
+		65360: "HOME",
+		65361: "LEFT",
+		65362: "UP",
+		65363: "RIGHT",
+		65364: "DOWN",
+		65367: "END",
+		65379: "INS",
+		65535: "DEL"
+		}
 
 # Python stdlib imports
 import os, signal, subprocess, sys
@@ -18,10 +36,24 @@ import gobject, gtk
 
 from subprocess import Popen, PIPE
 
+def loadMplayerInputConf():
+	for path in ("~/.mplayer/input.conf", '/usr/share/mplayer/input.conf'):
+		if os.path.exists(path):
+			fileContents = file(os.path.expanduser(path), 'r').read()
+			#TODO: Check whether it's worthwhile to optimize this.
+			keys = [x.strip().split('#')[0].strip().split(' ',1) for x in fileContents.split('\n') if not x.startswith('#')]
+			keys = [[y.strip() for y in x] for x in keys if len(x) == 2]			
+			return dict(keys)
+	return {}
+
 class Player(object):
 	def __init__(self, playlist):
 		self.playlist = playlist
-		self.filepath, self.child = '', None
+		self.filename, self.filepath = "", ''
+		self.child, self.fullscreen = None, False
+		
+		# Load the keyboard config
+		self.keyConfig = loadMplayerInputConf()
 	
 		# Set up the window
 		self.window = gtk.Window()
@@ -45,20 +77,46 @@ class Player(object):
 	def cb_window_close(self, widget, data=None):
 		if not self.child.poll():
 			os.kill(self.child.pid, signal.SIGTERM)
+			self.child.wait()
 		gtk.main_quit()
 
 	def cb_key_press(self, widget, event, data=None):
-		self.child.stdin.write(event.string)
+		#self.child.stdin.write(event.string)
+		if keySyms.has_key(event.keyval):
+			key = keySyms[event.keyval]
+		else:
+			print "\n\nUnknown KeySym: ", event.keyval, "\n\n"			
+			key = event.string
+			
+		if self.keyConfig.has_key(key):
+			action = self.keyConfig[key]
+			if action == 'quit':
+				self.window.destroy()
+			elif action == 'vo_fullscreen':
+				if self.fullscreen == False:
+					self.window.fullscreen()
+					self.fullscreen = True #FIXME: This doesn't track WM-based changes.
+				else:
+					self.window.unfullscreen()
+					self.fullscreen = False #FIXME: This doesn't track WM-based changes.
+			else:
+				self.child.stdin.write("%s\n" % action)
+		else:
+			print "\rNo Binding for '%s'\n" % key
 
 	def cb_tick(self, data=None):
 		if not self.child or self.child.poll() is not None:
 			# None = still running, negative = terminated by signal, positive = exit code
+			if self.filename:
+				fh = file(os.path.expanduser("~/.config/animu_played"), 'a')
+				fh.write('\n%s' % self.filename)
+				fh.close()
 			if self.playlist:
 				self.filepath = os.path.abspath(os.path.normpath(self.playlist.pop(0)))
 				self.filename, sockId = os.path.split(self.filepath)[1], str(self.socket.get_id())
 				
 				self.window.set_title(self.filename)
-				self.child = Popen(["mplayer", "-wid", sockId, self.filepath], stdin=PIPE)
+				self.child = Popen(["mplayer", "-slave", "-wid", sockId, self.filepath], stdin=PIPE)
 			else:
 				gtk.main_quit()
 		return True
@@ -66,10 +124,26 @@ class Player(object):
 def get_unplayed_contents(folderPath):
 	playedListFile = file(os.path.expanduser("~/.config/animu_played"), 'a+')
 	playedList = playedListFile.read().split('\n') #FIXME: This should be a list, not a journal.
-	return [os.path.join(folderPath, x) for x in sorted(os.listdir(folderPath)) if not os.path.join(folderPath, x) in playedList]
+	return [os.path.join(folderPath, x) for x in sorted(os.listdir(folderPath)) if not x in playedList]
+
+def playDirectory(dirPath):
+	playlist = get_unplayed_contents(dirPath)
+	if playlist:
+		pl = Player(playlist)
+		gtk.main()
+	else:
+		gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format="No un-watched episodes found.").run()
+		
 
 # Start the playing
-if len(sys.argv) >= 2:
+if len(sys.argv) >= 3 and '--force-dir' in sys.argv:
+	sys.argv.remove('--force-dir')
+	
+	temp = sys.argv[1]
+	while not os.path.isdir(temp):
+		temp = os.path.split(temp)[0]
+	playDirectory(temp)
+elif len(sys.argv) >= 2:
 	pl = Player(sys.argv[1:])
 	gtk.main()
 else:
@@ -78,6 +152,4 @@ else:
 	if dirPicker.run() == gtk.RESPONSE_ACCEPT:
 		chosenDir = dirPicker.get_filename()
 		dirPicker.destroy()
-		
-		pl = Player(get_unplayed_contents(chosenDir))
-		gtk.main()
+		playDirectory(chosenDir)
