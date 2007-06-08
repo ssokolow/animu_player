10a267a41aab98b2bab0b9ff553ca5c7a5910c40
Stephan Sokolow <via_bzr.zen.ssokolow@spamgourmet.com>
2007-06-08 01:38:08 -0400

    Made a bunch of corrections and began the preparations to pass key presses to the child MPlayer process.

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import pygtk
 pygtk.require('2.0')
 import gtk,sys
 
+# Set up the window
 window = gtk.Window()
+window.set_default_size(640,480)
 window.show()
 
 socket = gtk.Socket()
@@ -16,12 +18,16 @@ window.add(socket)
 print "Socket ID=", socket.get_id()
 window.connect("destroy", gtk.mainquit)
 
+def cb_key_press(widget, event, data):
+    print event #TODO: Hand the data off to the child MPlayer process.
+
 def plugged_event(widget):
     print "I (",widget,") have just had a plug inserted!" 
 
-    socket.connect("plug-added", plugged_event)
+socket.connect("plug-added", plugged_event)
+window.connect("key_press_event", cb_key_press)
 
-    if len(sys.argv) == 2:
-       socket.add_id(long(sys.argv[1]))
+if len(sys.argv) == 2:
+    socket.add_id(long(sys.argv[1]))
 
-gtk.mainloop()
+gtk.main()
